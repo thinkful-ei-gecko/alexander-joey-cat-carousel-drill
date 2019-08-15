4139ee655fc7427c4937c6c2e97bef4d8643816c
Alexander Fukui <alexander.fukui@gmail.com>
2019-08-15 13:25:37 -0500

    sync

--- cat drill/index.js
+++ cat drill/index.js
@@ -4,6 +4,7 @@ $('.thumbnail').on('click', e => {
   const imgSrc = $(event.currentTarget).find('img').attr('src');
   const imgAlt = $(event.currentTarget).find('img').attr('alt');
   $('.hero img').attr('src', imgSrc).attr('alt', imgAlt);
+
 });
 
 // $('.thumbnail').on('click', e => {
