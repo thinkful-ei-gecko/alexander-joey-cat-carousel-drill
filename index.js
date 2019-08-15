b3ac87ab425a53dfb40f2df7075a1c6f34957513
Alexander Fukui <alexander.fukui@gmail.com>
2019-08-15 13:23:19 -0500

    blah

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ $('.thumbnail').on('click', e => {
   const imgSrc = $(event.currentTarget).find('img').attr('src');
   const imgAlt = $(event.currentTarget).find('img').attr('alt');
   $('.hero img').attr('src', imgSrc).attr('alt', imgAlt);
-
 });
 
 // $('.thumbnail').on('click', e => {
